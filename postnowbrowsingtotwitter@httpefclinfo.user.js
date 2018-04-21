da00a3e245b4b91822ed8f3fe806b8d95b12172e
azu <azuciao@gmail.com>
2018-04-21 13:45:15 +0900

    1.4.0

--- postnowbrowsingtotwitter@httpefclinfo.user.js
+++ postnowbrowsingtotwitter@httpefclinfo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Post Now browsing to Twitter
 // @namespace http://efcl.info/
-// @version 1.3.1
+// @version 1.4.0
 // @description Usage: Ctrl + Shift + Enter -> "Now browsing: ****" on Twitter.
 // @include http://*
 // @include https://*
